5cc19cff3f889ef3b004c4156156ab41dcde6077
elja <elja_1989@mail.ru>
2012-02-01 04:02:58 -0800

    ImageMagick/Juggernaut custom installer

--- cookbooks/construcs_rake_starter/recipes/default.rb
+++ cookbooks/construcs_rake_starter/recipes/default.rb
@@ -15,18 +15,20 @@ execute "bundler install" do
   command "gem install bundler"
 end
 
-
 execute "create sym links for uploads" do
-  command "ln -nfs #{app_dir_shared}/uploads #{app_dir_current}/public/"
+  command "ln -nfs #{app_dir_shared}/uploads #{app_dir_current}/public/uploads"
 end
 
 execute "create sym links for sunspot" do
   command "ln -nfs #{app_dir_shared}/solr #{app_dir_current}/solr"
 end
 
-
 ey_cloud_report "starting sunspot" do
   message "starting sunspot"
+end
+
+execute "sunspot stop" do
+  command "cd #{app_dir_current} && RAILS_ENV=#{env} bundle exec rake sunspot:solr:stop"
 end
 
 execute "sunspot start" do
@@ -37,6 +39,9 @@ ey_cloud_report "starting delayed jobs" do
   message "starting delayed jobs"
 end
 
+execute "delayed job stop" do
+  command "cd #{app_dir_current} && RAILS_ENV=#{env} ./script/delayed_job stop"
+end
 
 execute "delayed job start" do
   command "cd #{app_dir_current} && RAILS_ENV=#{env} ./script/delayed_job start"
